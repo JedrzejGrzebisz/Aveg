5a863d7a3cd2ec828848eb4186e6f4781f1adb6e
Jedrzej Grzebisz <jg.24@interia.pl>
2020-08-19 12:13:30 +0200

    android studio update

--- app/src/main/java/com/example/aveg/MainActivity.java
+++ app/src/main/java/com/example/aveg/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-//add some comment
+//test do gita
 public class MainActivity extends AppCompatActivity {
 
     @Override
